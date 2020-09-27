6977b9086784ec459c6a62cfb04dabf7c50abb71
Andrew Siegel <siegelal7@gmail.com>
2020-09-27 19:09:07 -0400

    got all question prompts for various roles, and function to determine which role to make next-working on while loop

--- app.js
+++ app.js
@@ -10,9 +10,172 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const questions = [
+  "What is the team managers' name?",
+  "What is the engineers' name?",
+  "What is the interns' name?",
+  "Enter team members' email address:",
+  "Enter your team managers' office number:",
+  "Enter the team members' github username:",
+  "Enter your interns' school:",
+  "What role would you like to add next?",
+  "Enter team member ID number:",
+];
+const team = [];
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+// manager inquirer block
+function determineNextRoll() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: questions[7],
+        name: "position",
+        choices: ["Team Manager", "Engineer", "Intern", "No more members"],
+      },
+    ])
+    .then(function (data) {
+      //   console.log(data.name);
+      //   render(data);
+
+      if (data.position === "Team Manager") {
+        getInfoManager();
+      } else if (data.position === "Engineer") {
+        getInfoEngineer();
+      } else {
+        getInfoIntern();
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+determineNextRoll();
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
+
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above target this location.
+// Hint: you may need to check if the `output` folder exists and create it if it
+// does not.
+
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
+
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
+// and Intern classes should all extend from a class named Employee; see the directions
+// for further information. Be sure to test out each class and verify it generates an
+// object with the correct structure and methods. This structure will be crucial in order
+// for the provided `render` function to work! ```
+function getInfoManager() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: questions[0],
+        name: "name",
+      },
+      {
+        type: "input",
+        message: questions[8],
+        name: "ID",
+      },
+      {
+        type: "input",
+        message: questions[3],
+        name: "email",
+      },
+      {
+        type: "input",
+        message: questions[4],
+        name: "office",
+      },
+    ])
+    .then(function (data) {
+      //   console.log(data.name);
+      //   render(data);
+      team.push(data);
+      console.log(team);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+// getInfoManager();
+// console.log(team);
+// engineer inquirer block
+function getInfoEngineer() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: questions[1],
+        name: "name",
+      },
+      {
+        type: "input",
+        message: questions[8],
+        name: "ID",
+      },
+      {
+        type: "input",
+        message: questions[3],
+        name: "email",
+      },
+      {
+        type: "input",
+        message: questions[5],
+        name: "github",
+      },
+    ])
+    .then(function (data) {
+      //   console.log(data.name);
+      team.push(data);
+      console.log(team);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+//intern inquirer block
+function getInfoIntern() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: questions[2],
+        name: "name",
+      },
+      {
+        type: "input",
+        message: questions[8],
+        name: "ID",
+      },
+      {
+        type: "input",
+        message: questions[3],
+        name: "email",
+      },
+      {
+        type: "input",
+        message: questions[6],
+        name: "school",
+      },
+    ])
+    .then(function (data) {
+      //   console.log(data.name);
+      team.push(data);
+      console.log(team);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
