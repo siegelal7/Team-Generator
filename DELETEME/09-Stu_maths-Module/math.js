f9b7a1d4e54eb322ac9eb1f0873fdf9929948550
Andrew Siegel <siegelal7@gmail.com>
2020-09-27 22:04:37 -0400

    stuck w rendering html

---
+++ DELETEME/09-Stu_maths-Module/math.js
@@ -0,0 +1,20 @@
+function sum(numOne, numTwo) {
+  console.log(numOne + numTwo);
+}
+function sub(numOne, numTwo) {
+  console.log(numOne - numTwo);
+}
+function mult(numOne, numTwo) {
+  console.log(numOne * numTwo);
+}
+function divide(numOne, numTwo) {
+  console.log(numOne / numTwo);
+}
+
+module.exports = {
+  //   math: math,
+  sum: sum,
+  sub: sub,
+  mult: mult,
+  divide: divide,
+};
--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+// const renderHtml = require("renderHtml");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -88,7 +89,7 @@ function getInfoManager() {
       {
         type: "input",
         message: questions[8],
-        name: "ID",
+        name: "id",
       },
       {
         type: "input",
@@ -104,7 +105,9 @@ function getInfoManager() {
     .then(function (data) {
       //   console.log(data.name);
       //   render(data);
-      team.push(data);
+      const person = new Manager(data.name, data.id, data.email, data.office);
+      console.log(person);
+      team.push(person);
       console.log(team);
       determineNextRoll();
     })
@@ -126,7 +129,7 @@ function getInfoEngineer() {
       {
         type: "input",
         message: questions[8],
-        name: "ID",
+        name: "id",
       },
       {
         type: "input",
@@ -141,7 +144,8 @@ function getInfoEngineer() {
     ])
     .then(function (data) {
       //   console.log(data.name);
-      team.push(data);
+      const person = new Engineer(data.name, data.id, data.email, data.github);
+      team.push(person);
       console.log(team);
       determineNextRoll();
     })
@@ -161,7 +165,7 @@ function getInfoIntern() {
       {
         type: "input",
         message: questions[8],
-        name: "ID",
+        name: "id",
       },
       {
         type: "input",
@@ -176,7 +180,8 @@ function getInfoIntern() {
     ])
     .then(function (data) {
       //   console.log(data.name);
-      team.push(data);
+      const person = new Intern(data.name, data.id, data.email, data.school);
+      team.push(person);
       console.log(team);
       determineNextRoll();
     })
